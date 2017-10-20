73a60b6ed3005e13f6f8d356d588e8d301e9914f
Lutfi Demirci <lutfidemirci@gmail.com>
2017-10-20 21:31:51 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      omniauth_callbacks: 'users/omniauth_callbacks'
+    }
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
