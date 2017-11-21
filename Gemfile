ad038fe1672074489e94f986c1f3e97d3fca6807
Lutfi Demirci <lutfidemirci@gmail.com>
2017-11-21 18:17:32 +0300

    Added user devise model

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Devise
+gem 'devise'
+gem 'omniauth'
+gem 'omniauth-github'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # , :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:github]
+
+ def self.from_omniauth(auth)
+   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+     user.email = auth.info.email
+     user.password = Devise.friendly_token[0,20]
+     user.skip_confirmation!
+   end
+ end
+end
