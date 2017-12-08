228d114186230f8a62322590372da74730f8285d
Umutcan Karakaş <umutcankarakas@gmail.com>
2017-12-08 20:02:21 +0300

    Testlerle ilgili eklemeler
    -Bütün testlerin patlamasına sebep olan test\fixtures\users.yml deki bir satır commente alındı.
    -pomodoro_controller_test ile pomodoro sayfalarının başlıkları test edildi

--- test/controllers/pomodoro_controller_test.rb
+++ test/controllers/pomodoro_controller_test.rb
@@ -4,26 +4,31 @@ class PomodoroControllerTest < ActionDispatch::IntegrationTest
   test "should get start" do
     get pomodoro_start_url
     assert_response :success
+    assert_select "title", "Gurupomodoro | "
   end
 
   test "should get begin" do
     get pomodoro_begin_url
     assert_response :success
+    assert_select "title", "Gurupomodoro | Working"
   end
 
   test "should get break_start" do
     get pomodoro_break_start_url
     assert_response :success
+    assert_select "title", "Gurupomodoro | Break"
   end
 
   test "should get interrupt" do
     get pomodoro_interrupt_url
     assert_response :success
+    assert_select "title", "Gurupomodoro | Interrupt"
   end
 
   test "should get finished" do
     get pomodoro_finished_url
     assert_response :success
+    assert_select "title", "Gurupomodoro | Finished"
   end
 
 end
