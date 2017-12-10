b133dae34b4dce662940c7a5469ed22f29a30e20
Umutcan Karakaş <umutcankarakas@gmail.com>
2017-12-10 22:32:02 +0300

    test/unit gemi eklemesi ve basit testler

--- test/controllers/pomodoro_controller_test.rb
+++ test/controllers/pomodoro_controller_test.rb
@@ -1,34 +1,15 @@
 require 'test_helper'
 
-class PomodoroControllerTest < ActionDispatch::IntegrationTest
-  test "should get start" do
-    get pomodoro_start_url
-    assert_response :success
-    assert_select "title", "Gurupomodoro | "
+class PomodoroControllerTest < Test::Unit::TestCase
+  def test_true_is_true
+    assert true
   end
 
-  test "should get begin" do
-    get pomodoro_begin_url
-    assert_response :success
-    assert_select "title", "Gurupomodoro | Working"
-  end
-
-  test "should get break_start" do
-    get pomodoro_break_start_url
-    assert_response :success
-    assert_select "title", "Gurupomodoro | Break"
-  end
-
-  test "should get interrupt" do
-    get pomodoro_interrupt_url
-    assert_response :success
-    assert_select "title", "Gurupomodoro | Interrupt"
-  end
-
-  test "should get finished" do
-    get pomodoro_finished_url
-    assert_response :success
-    assert_select "title", "Gurupomodoro | Finished"
+  def test_true_is_true
+    assert false
   end
 
 end
+
+
+
